3b08fcd3a84269d99323f4f7c4b8cfe662ccf71a
shibbu04 <shivamsingh57680@gmail.com>
2025-02-06 22:06:06 +0530

    fixed env issue

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { useAuthStore } from '../store/auth.store';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
